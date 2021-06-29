e73c07063c1e8e96e95bcee529a36e346de6a898
othermod <22604540+othermod@users.noreply.github.com>
2021-06-29 13:16:18 -0400

    add WiFi enable/disable scripts

---
+++ PSPi/Scripts/(1) Disable WiFi + BT Hardware and Reboot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Checking status in config.txt"
+grep '#dtoverlay=pi3-disable-wifi' /boot/config.txt >/dev/null
+if [ $? -eq 0 ]; then
+	echo "Disabling WiFi and BT in config.txt"
+	sudo sed -i 's/#dtoverlay=pi3-disable-wifi/dtoverlay=pi3-disable-wifi/' /boot/config.txt
+	sudo sed -i 's/#dtoverlay=pi3-disable-bt/dtoverlay=pi3-disable-bt/' /boot/config.txt
+	echo "Disabling services"
+	sudo systemctl disable wpa_supplicant 2> /dev/null
+	sudo systemctl disable bluetooth 2> /dev/null
+	sudo systemctl disable hciuart 2> /dev/null
+	echo "Rebooting"
+	sleep 1
+	sudo reboot
+else
+	echo "WiFi and BT appear to already be disabled"
+	sleep 1
+fi
