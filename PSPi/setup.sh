25f6da8fb6e3468fb0b665c0a0e66bdf3b5a8ce5
othermod <22604540+othermod@users.noreply.github.com>
2021-07-09 14:14:49 -0400

    update setup script

--- PSPi/setup.sh
+++ PSPi/setup.sh
@@ -6,7 +6,7 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1
 fi
 
-killall emulationstatio
+killall emulationstatio 2>/dev/null
 sleep 1
 echo "Copying files"
 # add boot script to rc.local
@@ -14,15 +14,15 @@ grep pspi /etc/rc.local >/dev/null
 if [ $? -eq 0 ]; then
 #	echo "pspi already exists in rc.local. Re-creating."
 	# pspi already in rc.local, but make sure correct:
-	sed -i "s/^.*pspi.*$/bash \/boot\/PSPi\/boot.sh/g" /etc/rc.local >/dev/null
+	sed -i "s/^.*pspi.*$/bash \/boot\/pspi\/boot.sh/g" /etc/rc.local >/dev/null
 else
 #	echo "pspi doesn't exist in rc.local. Creating."
 	# Insert pspi into rc.local before final 'exit 0'
-	sed -i "s/^exit 0/bash \/boot\/PSPi\/boot.sh\\nexit 0/g" /etc/rc.local >/dev/null
+	sed -i "s/^exit 0/bash \/boot\/pspi\/boot.sh\\nexit 0/g" /etc/rc.local >/dev/null
 fi
 
 # Make directory for driver and other files
-rm -r /home/pi/PSPi >/dev/null
+rm -r /home/pi/PSPi 2>/dev/null
 mkdir /home/pi/PSPi
 
 # Startup image
@@ -49,8 +49,32 @@ cp -r /boot/PSPi/Console/. /etc/emulationstation/themes/carbon/PSPi/
 cp -f /boot/pspi/Theme/background.png /etc/emulationstation/themes/carbon/art/carbon_fiber.png
 cp -f /boot/PSPi/Theme/carbon.xml /etc/emulationstation/themes/carbon/carbon.xml
 
+# Remove verbose startup/shutdown to shave time off startup
+grep fastboot /boot/cmdline.txt >/dev/null
+if [ $? -eq 0 ]; then
+        echo "Found"
+        # pspi already in rc.local, but make sure correct:
+        #sed -i "s/^.*pspi.*$/bash \/boot\/pspi\/boot.sh/g" /etc/rc.local >/dev/null
+else
+        echo "Not Found, adding"
+        # Insert pspi into rc.local before final 'exit 0'
+        sed -i ' 1 s/.*/& fastboot/' /boot/cmdline.txt >/dev/null
+fi
+grep quiet /boot/cmdline.txt >/dev/null
+if [ $? -eq 0 ]; then
+        echo "Found"
+        # pspi already in rc.local, but make sure correct:
+        #sed -i "s/^.*pspi.*$/bash \/boot\/pspi\/boot.sh/g" /etc/rc.local >/dev/null
+else
+        echo "Not Found, adding"
+        # Insert pspi into rc.local before final 'exit 0'
+        sed -i ' 1 s/.*/& quiet/' /boot/cmdline.txt >/dev/null
+fi
+
+
+
 #remove DHCP wait, for faster bootup
-rm -f /etc/systemd/system/dhcpcd.service.d/wait.conf
+rm -f /etc/systemd/system/dhcpcd.service.d/wait.conf 2>/dev/null
 sleep 1
 echo "Final Step --> select Interface Options, I2C, then enable I2C and reboot"
 sleep 1
