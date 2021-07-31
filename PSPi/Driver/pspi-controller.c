9c8615e679ebbd6af7b40f06bcbda4ad0eeaebd9
othermod <22604540+othermod@users.noreply.github.com>
2021-07-31 12:59:30 -0400

    updated driver to make actions more clear in code, and to fix indicator placement on 480x272 LCD

--- PSPi/Driver/pspi-controller.c
+++ PSPi/Driver/pspi-controller.c
@@ -1,174 +1,172 @@
 // http://elinux.org/Interfacing_with_I2C_Devices
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "I2C.h"
 #include "JoystickDevice.h"
-#include <unistd.h>
-
 
 #define I2C_GAMEPAD_ADDRESS 0x18
-#define UPDATE_FREQ 16666 // ms (60Hz)
+#define UPDATE_FREQ 1000000/60 // ms (60Hz)
+
+const int magicNumber = 17; // Number that corrects for internal resistance on LiPo battery
+const int rolling = 64; // Number of readings being averaged together. This has to match the atmega variable
+_Bool isCharging = 0;
+_Bool previousIsCharging = 0;
+_Bool isMute = 0;
+_Bool previousIsMute = 0;
+int line = 0;
+int chargeStatus = 11;
+int previousChargeStatus = 0;
+int indicationVoltage = 0;
+int rollingVoltage = 0;
+int amperageDifference = 0;
+int calculatedVoltage = 0;
+int resolution;
+uint16_t rawVolt;
+uint16_t rawAmp;
 
 typedef struct {
-  uint16_t buttons; // button status
-  uint8_t axis0; // first axis
-  uint8_t axis1; // second axis
-  uint16_t voltage; // raw voltage
-  uint16_t amperage; // raw amperage
-
+	uint16_t buttons; // button status
+	uint8_t axis0; // first axis
+	uint8_t axis1; // second axis
+	uint16_t voltage; // raw voltage
+	uint16_t amperage; // raw amperage
 } I2CJoystickStatus;
 
 int readI2CJoystick(int file, I2CJoystickStatus *status) {
-  int s = readI2CSlave(file, I2C_GAMEPAD_ADDRESS, status, sizeof(I2CJoystickStatus));
-  if(s != sizeof(I2CJoystickStatus))
-    return -1; // error
-
-  return 0; // no error
+	int s = readI2CSlave(file, I2C_GAMEPAD_ADDRESS, status, sizeof(I2CJoystickStatus));
+	if(s != sizeof(I2CJoystickStatus))
+		return -1; // error
+	return 0; // no error
 }
 
 #define TestBitAndSendKeyEvent(oldValue, newValue, bit, event) if((oldValue & (1 << bit)) != (newValue & (1 << bit))) sendInputEvent(UInputFIle, EV_KEY, event, (newValue & (1 << bit)) == 0 ? 0 : 1);
 
-void updateUInputDevice(int UInputFIle, I2CJoystickStatus *newStatus, I2CJoystickStatus *status) {
-  // update button event
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x00, BTN_A);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x01, BTN_B);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x02, BTN_X);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x03, BTN_Y);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x04, BTN_TL);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x05, BTN_TR);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x06, BTN_SELECT);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x07, BTN_START);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x08, BTN_DPAD_UP);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x09, BTN_DPAD_DOWN);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0A, BTN_DPAD_LEFT);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0B, BTN_DPAD_RIGHT);
-  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0C, BTN_1);
-//  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0D, BTN_2);
-//  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0E, BTN_3);
-//  TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0F, BTN_4);
-  // joystick axis
-  uint8_t val = newStatus->axis0;
-  if (val > 107 & val < 147) {val = 127;}
-  if (val != status->axis0) {sendInputEvent(UInputFIle, EV_ABS, ABS_X, val);}
-  val = newStatus->axis1;
-  if (val > 107 & val < 147) {val = 127;}
-  if (val != status->axis1) {sendInputEvent(UInputFIle, EV_ABS, ABS_Y, val);}
+void updateButtons(int UInputFIle, I2CJoystickStatus *newStatus, I2CJoystickStatus *status) {
+	// update button event
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x00, BTN_A);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x01, BTN_B);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x02, BTN_X);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x03, BTN_Y);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x04, BTN_TL);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x05, BTN_TR);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x06, BTN_SELECT);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x07, BTN_START);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x08, BTN_DPAD_UP);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x09, BTN_DPAD_DOWN);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0A, BTN_DPAD_LEFT);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0B, BTN_DPAD_RIGHT);
+	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0C, BTN_1);
+	// TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0D, BTN_2);
+	// TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0E, BTN_3);
+	// TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0F, BTN_4);
+	uint8_t joystickValue = newStatus->axis0;
+	if (joystickValue > 107 & joystickValue < 147) {joystickValue = 127;}
+	if (joystickValue != status->axis0) {sendInputEvent(UInputFIle, EV_ABS, ABS_X, joystickValue);}
+	joystickValue = newStatus->axis1;
+	if (joystickValue > 107 & joystickValue < 147) {joystickValue = 127;}
+	if (joystickValue != status->axis1) {sendInputEvent(UInputFIle, EV_ABS, ABS_Y, joystickValue);}
+}
+
+void updateJoystick(int UInputFIle, I2CJoystickStatus *newStatus, I2CJoystickStatus *status) {
+
+}
+
+void startLog() {
+	FILE * fp;
+	fp = fopen ("log.csv","w");
+	fprintf (fp, "Line,rollingVoltage,amperageDifference,calculatedVoltage,indicationVoltage\n");
+	fclose(fp);	
+}
+
+void writeLog() {
+	FILE * fp;
+	fp = fopen ("log.csv","a");
+	line++;
+	fprintf (fp, "%d,%d,%d,%d,%d",line,rollingVoltage,amperageDifference,calculatedVoltage,indicationVoltage);
+	fclose(fp);
+}
+
+void readController() {
+}
+
+void calculateBattery() {
+	// a lot of this math is just converting the raw values to readable voltage
+	// test efficiency to see whether it is worth working with raw values instead
+	rollingVoltage = rawVolt * 11 * 3300 / 1024 / rolling;
+	amperageDifference = (rawVolt - rawAmp) * 10 / 11;
+	calculatedVoltage = rollingVoltage + amperageDifference * 10 / magicNumber;
+	previousIsCharging = isCharging;
+	if (indicationVoltage == 0) {indicationVoltage = calculatedVoltage;}
+	if (isCharging == 0) {
+		if (calculatedVoltage < indicationVoltage) { indicationVoltage--;}
+		if (amperageDifference < 10 || rollingVoltage > 4200) {isCharging = 1;}
+	} else {
+		if (calculatedVoltage > indicationVoltage) { indicationVoltage++;}
+		if (amperageDifference > 100) {isCharging = 0;}
+	}
+	previousChargeStatus = chargeStatus;
+	chargeStatus = 0;
+	if (indicationVoltage > 3478) {chargeStatus = 1;}
+	if (indicationVoltage > 3549) {chargeStatus = 2;}
+	if (indicationVoltage > 3619) {chargeStatus = 3;}
+	if (indicationVoltage > 3655) {chargeStatus = 4;}
+	if (indicationVoltage > 3725) {chargeStatus = 5;}
+	if (indicationVoltage > 3761) {chargeStatus = 6;}
+	if (indicationVoltage > 3866) {chargeStatus = 7;}
+	if (indicationVoltage > 3927) {chargeStatus = 8;}
+	if (indicationVoltage > 4027) {chargeStatus = 9;}
+	if (indicationVoltage > 4175) {chargeStatus = 99;}
+	if ((previousChargeStatus != chargeStatus) || (previousIsCharging != isCharging) || (previousIsMute != isMute)) {
+		// Changing Battery Status
+		char temp[512];
+		system ("sudo killall pngview");
+		sprintf(temp, "/home/pi/PSPi/Driver/./pngview -n -b 0 -l 100000 -x %d -y 2 /home/pi/PSPi/Driver/PNG/battery%d%d%d.png &",resolution - 46,isMute,isCharging,chargeStatus);
+		system((char *)temp);
+	}
 }
 
 int main(int argc, char *argv[]) {
-  // open I2C device
-  int I2CFile = openI2C(1);
-
-  // current joystick status
-  I2CJoystickStatus status;
-  status.buttons = 0;
-  status.axis0 = 0;
-  status.axis1 = 0;
-
-  // create uinput device
-  int UInputFIle = createUInputDevice();
-
-  // check for I2C connection and attempt to update firmware if not detected
-  printf("PSPi Controller Starting\n");
-  I2CJoystickStatus newStatus;
-   if(readI2CJoystick(I2CFile, &newStatus) != 0) {
-
-      printf("Controller is not detected on the I2C bus.\n");
-	  sleep(1);
-    }
-
-sleep(1);
-
-const int MagicNumber = 17;
-int IsCharging = 0;
-int PreviousIsCharging = 0;
-int ChargeStatus = 11;
-int PreviousChargeStatus = 0;
-int IsMute = 0;
-int PreviousIsMute = 0;
-//AVGvolt and AVGamp are the ADC readings averaged over the most recent 64 readings
-uint16_t AVGvolt = status.voltage;
-uint16_t AVGamp = status.amperage;
-
-//rolling is the number of readings being averaged together
-const int rolling = 64;
-
-int IndicationVoltage = 0;
-
-//FILE * fp;
-//fp = fopen ("log.csv","w");
-//fprintf (fp, "Line,RollingVoltage,AmperageDifference,CalculatedVoltage,IndicationVoltage\n");
-//fclose(fp);
-//int line = 0;
-
-system("/home/pi/PSPi/Driver/./pngview -n -b 0 -l 100000 -x 754 -y 2 /home/pi/PSPi/Driver/PNG/batterystart.png &");
-  while(1) {
-    // read new status from I2C
-    I2CJoystickStatus newStatus;
-    if(readI2CJoystick(I2CFile, &newStatus) != 0) {
+	int I2CFile = openI2C(1); // open I2C device
+	I2CJoystickStatus status; // current joystick status
+	status.buttons = 0;
+	status.axis0 = 0;
+	status.axis1 = 0;
+	int UInputFIle = createUInputDevice(); // create uinput device
+	printf("PSPi Controller Starting\n");
+	I2CJoystickStatus newStatus;
+	if(readI2CJoystick(I2CFile, &newStatus) != 0) { // check for I2C connection
 		printf("Controller is not detected on the I2C bus.\n");
 		sleep(1);
-    } else {
-		// everything is ok
-		updateUInputDevice(UInputFIle, &newStatus, &status);
-		status = newStatus;
     }
-	AVGvolt = status.voltage;
-	AVGamp = status.amperage;
-	int RollingVoltage = AVGvolt * 11 * 3300 / 1024 / rolling;
-	int AmperageDifference = (AVGvolt - AVGamp) * 10 / 11;
-	int CalculatedVoltage = RollingVoltage + AmperageDifference * 10 / MagicNumber;
-	PreviousIsCharging = IsCharging;
-	if (IndicationVoltage == 0) {IndicationVoltage = CalculatedVoltage;}
-	if (IsCharging == 0) {
-		//printf("\nDischarging");
-		if (CalculatedVoltage < IndicationVoltage) { IndicationVoltage--;}
-		if (AmperageDifference < 10 || RollingVoltage > 4200) {IsCharging = 1;}
-	} else {
-		//printf("\nCharging");
-		if (CalculatedVoltage > IndicationVoltage) { IndicationVoltage++;}
-		if (AmperageDifference > 100) {IsCharging = 0;}
+	sleep(1);
+	// stores horizontal resolution to variable so this works with both LCD types
+	// would be better to grab it directly, but I'll have to work out the method
+	FILE *f = fopen("pspi.cfg","r");
+	int i;
+	char buf[4];
+	for (i = 0 ; i != 0 ; i++) {
+    		fgets(buf, 4, f);
 	}
-		PreviousChargeStatus = ChargeStatus;
-		ChargeStatus = 0;
-		if (IndicationVoltage > 3478) {ChargeStatus = 1;}
-		if (IndicationVoltage > 3549) {ChargeStatus = 2;}
-		if (IndicationVoltage > 3619) {ChargeStatus = 3;}
-		if (IndicationVoltage > 3655) {ChargeStatus = 4;}
-		if (IndicationVoltage > 3725) {ChargeStatus = 5;}
-		if (IndicationVoltage > 3761) {ChargeStatus = 6;}
-		if (IndicationVoltage > 3866) {ChargeStatus = 7;}
-		if (IndicationVoltage > 3927) {ChargeStatus = 8;}
-		if (IndicationVoltage > 4027) {ChargeStatus = 9;}
-		if (IndicationVoltage > 4175) {ChargeStatus = 99;}
-		PreviousIsMute = IsMute;
-		IsMute = (status.buttons >> 0x0F) & 1;
-		if ((PreviousChargeStatus != ChargeStatus) || (PreviousIsCharging != IsCharging) || (PreviousIsMute != IsMute)) {
-			//printf("\nChanging Battery Status");
-			char temp[512];
-			//sprintf(temp, "/home/pi/PSPi/Driver/./pngviewtemp -n -b 0 -l 100000 -x 754 -y 2 /home/pi/PSPi/Driver/PNG/battery%d%d%d.png &",IsMute,IsCharging,ChargeStatus);
-			//system((char *)temp);
-			system ("sudo killall pngview");
-			sprintf(temp, "/home/pi/PSPi/Driver/./pngview -n -b 0 -l 100000 -x 754 -y 2 /home/pi/PSPi/Driver/PNG/battery%d%d%d.png &",IsMute,IsCharging,ChargeStatus);
-			system((char *)temp);
-//		system ("sudo killall pngviewtemp");
+	fscanf(f, "%d", &resolution);
+
+	while(1) {
+	    I2CJoystickStatus newStatus; // read new status from I2C
+		if(readI2CJoystick(I2CFile, &newStatus) != 0) {
+			printf("Controller is not detected on the I2C bus.\n");
+			sleep(1);
+		} else { // everything is ok
+			updateButtons(UInputFIle, &newStatus, &status);
+			status = newStatus;
 		}
+		rawVolt = status.voltage;
+		rawAmp = status.amperage;
+		previousIsMute = isMute;
+		isMute = (status.buttons >> 0x0F) & 1;
+		calculateBattery();
+		usleep(UPDATE_FREQ);
+	}
 
-//		FILE * fp;
-//		fp = fopen ("log.csv","a");
-//		line++;
-//		fprintf (fp, "%d",line);
-//		fprintf (fp, ",%d",RollingVoltage);
-//		fprintf (fp, ",%d",AmperageDifference);
-//		fprintf (fp, ",%d",CalculatedVoltage);
-//		fprintf (fp, ",%d\n",IndicationVoltage);
-//		fclose(fp);
-
-    // sleep until next update
-    usleep(UPDATE_FREQ);
-  }
-
-  // close file
-  close(I2CFile);
-  ioctl(UInputFIle, UI_DEV_DESTROY);
+	close(I2CFile); // close file
+	ioctl(UInputFIle, UI_DEV_DESTROY);
 }
--- PSPi/boot.sh
+++ PSPi/boot.sh
@@ -1,1 +1,6 @@
+
+# check resolution and create config file
+cat /sys/class/graphics/fb0/virtual_size | cut -d, -f1 > pspi.cfg
+mv pspi.cfg /home/pi/PSPi/Driver/pspi.cfg
+
 sudo /home/pi/PSPi/Driver/./pspi-controller &
