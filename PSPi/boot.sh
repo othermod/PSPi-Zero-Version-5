a0ff2ca2ffe8e7aa39110f8c2290f721bf34bf81
othermod <22604540+othermod@users.noreply.github.com>
2021-07-31 22:45:54 -0400

    update boot script

--- PSPi/boot.sh
+++ PSPi/boot.sh
@@ -1,7 +1,6 @@
-
+cd /home/pi/PSPi/Driver
 # check resolution and create config file
 cat /sys/class/graphics/fb0/virtual_size | cut -d, -f1 > pspi.cfg
-mv pspi.cfg /home/pi/PSPi/Driver/pspi.cfg
 
-cd /home/pi/PSPi/Driver
+# start driver
 sudo ./pspi-controller &
