bdcdf22bc1ee78f74b1472ccc31895ef126eb4a3
othermod <22604540+othermod@users.noreply.github.com>
2021-08-10 16:51:49 -0400

    fix formatting

--- PSPi/Driver/pspi-controller.c
+++ PSPi/Driver/pspi-controller.c
@@ -1,4 +1,3 @@
-// http://elinux.org/Interfacing_with_I2C_Devices
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,7 +10,8 @@
 #include <stdint.h>
 
 #define I2C_GAMEPAD_ADDRESS 0x18
-#define UPDATE_FREQ 1000000/60 // ms (60Hz)
+#define REFRESH_RATE 60 // refresh rate in Hz
+#define UPDATE_FREQ 1000000/REFRESH_RATE
 
 const int magicNumber = 17; // Number that corrects for internal resistance on LiPo battery
 const int rolling = 64; // Number of readings being averaged together. This has to match the atmega variable
@@ -31,119 +31,95 @@ uint16_t rawVolt;
 uint16_t rawAmp;
 
 int openI2C() { 
-  int file;
-  char filename[2048];
-//specify which I2C bus to use
-  sprintf(filename, "/dev/i2c-1");
-
-  if ((file = open(filename, O_RDWR)) < 0) {
-    /* ERROR HANDLING: you can check errno to see what went wrong */
-    fprintf(stderr, "Failed to open the i2c bus");
-    exit(1);
-  }
-  return file;
+	int file;
+	char filename[2048];
+	sprintf(filename, "/dev/i2c-1"); //specify which I2C bus to use
+	if ((file = open(filename, O_RDWR)) < 0) {
+		fprintf(stderr, "Failed to open the i2c bus"); /* ERROR HANDLING: you can check errno to see what went wrong */
+		exit(1);
+	}
+	return file;
 }
 
 int readI2CSlave(int file, int slaveAddress, void *buf, size_t count) {
-  // initialize communication
-  if (ioctl(file, I2C_SLAVE, slaveAddress) < 0) {
-    fprintf(stderr, "I2C: Failed to acquire bus access/talk to slave 0x%x\n", slaveAddress);
-    return 0;
-  }
-  int s = 0;  
-  s = read(file, buf, count);
-  return s; // no error
+	if (ioctl(file, I2C_SLAVE, slaveAddress) < 0) { // initialize communication
+		fprintf(stderr, "I2C: Failed to acquire bus access/talk to slave 0x%x\n", slaveAddress);
+		return 0;
+	}
+	int s = 0;  
+	s = read(file, buf, count);
+	return s; // no error
 }
 
 int createUInputDevice() {
-  int fd;
-
-  fd = open("/dev/uinput", O_WRONLY | O_NDELAY);
-  if(fd < 0) {
-    fprintf(stderr, "Can't open uinput device!\n");
-    exit(1);
-  }
-  
+	int fd;
+	fd = open("/dev/uinput", O_WRONLY | O_NDELAY);
+	if(fd < 0) {
+		fprintf(stderr, "Can't open uinput device!\n");
+		exit(1);
+	}
     // device structure
-  struct uinput_user_dev uidev;
-  memset(&uidev, 0, sizeof(uidev));
-
-  // init event  
-  int ret = 0;
-  ret |= ioctl(fd, UI_SET_EVBIT, EV_KEY);
-  ret |= ioctl(fd, UI_SET_EVBIT, EV_REL);
-
-  // button
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_A);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_B);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_X);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_Y);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_TL);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_TR);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_SELECT);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_START);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_DPAD_UP);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_DPAD_DOWN);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_DPAD_LEFT);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_DPAD_RIGHT);
-  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_1);
-//  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_2);
-//  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_3);
-//  ret |= ioctl(fd, UI_SET_KEYBIT, BTN_4);
-
-
-  // axis
-  ret |= ioctl(fd, UI_SET_EVBIT, EV_ABS);
-  ret |= ioctl(fd, UI_SET_ABSBIT, ABS_X);
-  uidev.absmin[ABS_X] = 55;
-  uidev.absmax[ABS_X] = 200;
-  
-  ret |= ioctl(fd, UI_SET_ABSBIT, ABS_Y);
-  uidev.absmin[ABS_Y] = 55;
-  uidev.absmax[ABS_Y] = 200;
-
-  
-  if(ret) {
-    fprintf(stderr, "Error while configuring uinput device!\n");
-    exit(1);
-  }
-
-  snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "PSPi Controller");
-  uidev.id.bustype = BUS_USB;
-  uidev.id.vendor  = 1;
-  uidev.id.product = 5;
-  uidev.id.version = 1;
-
-  ret = write(fd, &uidev, sizeof(uidev));
-  if(ioctl(fd, UI_DEV_CREATE)) {
-    fprintf(stderr, "Error while creating uinput device!\n");
-    exit(1);    
-  }
-
-  return fd;
+	struct uinput_user_dev uidev;
+	memset(&uidev, 0, sizeof(uidev));
+	// init event  
+	int ret = 0;
+	ret |= ioctl(fd, UI_SET_EVBIT, EV_KEY);
+	ret |= ioctl(fd, UI_SET_EVBIT, EV_REL);
+	// button
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_A);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_B);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_X);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_Y);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_TL);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_TR);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_SELECT);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_START);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_DPAD_UP);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_DPAD_DOWN);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_DPAD_LEFT);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_DPAD_RIGHT);
+	ret |= ioctl(fd, UI_SET_KEYBIT, BTN_1);
+	//ret |= ioctl(fd, UI_SET_KEYBIT, BTN_2);
+	//ret |= ioctl(fd, UI_SET_KEYBIT, BTN_3);
+	//ret |= ioctl(fd, UI_SET_KEYBIT, BTN_4);
+	// axis
+	ret |= ioctl(fd, UI_SET_EVBIT, EV_ABS);
+	ret |= ioctl(fd, UI_SET_ABSBIT, ABS_X);
+	uidev.absmin[ABS_X] = 55;
+	uidev.absmax[ABS_X] = 200;
+	ret |= ioctl(fd, UI_SET_ABSBIT, ABS_Y);
+	uidev.absmin[ABS_Y] = 55;
+	uidev.absmax[ABS_Y] = 200;
+	if(ret) {
+		fprintf(stderr, "Error while configuring uinput device!\n");
+		exit(1);
+	}
+	snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "PSPi Controller");
+	uidev.id.bustype = BUS_USB;
+	uidev.id.vendor  = 1;
+	uidev.id.product = 5;
+	uidev.id.version = 1;
+	ret = write(fd, &uidev, sizeof(uidev));
+	if(ioctl(fd, UI_DEV_CREATE)) {
+		fprintf(stderr, "Error while creating uinput device!\n");
+		exit(1);    
+	}
+	return fd;
 }
 
 void sendInputEvent(int fd, uint16_t type, uint16_t code, int32_t value) {
-  struct input_event ev;
-
-  memset(&ev, 0, sizeof(ev));
-
-  ev.type = type;
-  ev.code = code;
-  ev.value = value;
-  
-  if(write(fd, &ev, sizeof(ev)) < 0) {
-    fprintf(stderr, "Error while sending event to uinput device!\n");
-  }
-
-  // need to send a sync event
-  ev.type = EV_SYN;
-  ev.code = SYN_REPORT;
-  ev.value = 0;
-  write(fd, &ev, sizeof(ev));
-  if (write(fd, &ev, sizeof(ev)) < 0) {
-    fprintf(stderr, "Error while sending event to uinput device!\n");
-  }
+	struct input_event ev;
+	memset(&ev, 0, sizeof(ev));
+	ev.type = type;
+	ev.code = code;
+	ev.value = value;
+	if(write(fd, &ev, sizeof(ev)) < 0) {fprintf(stderr, "Error while sending event to uinput device!\n");}
+	// need to send a sync event
+	ev.type = EV_SYN;
+	ev.code = SYN_REPORT;
+	ev.value = 0;
+	write(fd, &ev, sizeof(ev));
+	if (write(fd, &ev, sizeof(ev)) < 0) {fprintf(stderr, "Error while sending event to uinput device!\n");}
 }
 
 typedef struct {
@@ -178,19 +154,15 @@ void updateButtons(int UInputFIle, I2CJoystickStatus *newStatus, I2CJoystickStat
 	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0A, BTN_DPAD_LEFT);
 	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0B, BTN_DPAD_RIGHT);
 	TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0C, BTN_1);
-	// TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0D, BTN_2);
-	// TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0E, BTN_3);
-	// TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0F, BTN_4);
+	//TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0D, BTN_2);
+	//TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0E, BTN_3);
+	//TestBitAndSendKeyEvent(status->buttons, newStatus->buttons, 0x0F, BTN_4);
 	uint8_t joystickValue = newStatus->axis0;
 	if (joystickValue > 107 & joystickValue < 147) {joystickValue = 127;}
 	if (joystickValue != status->axis0) {sendInputEvent(UInputFIle, EV_ABS, ABS_X, joystickValue);}
 	joystickValue = newStatus->axis1;
 	if (joystickValue > 107 & joystickValue < 147) {joystickValue = 127;}
 	if (joystickValue != status->axis1) {sendInputEvent(UInputFIle, EV_ABS, ABS_Y, joystickValue);}
-}
-
-void updateJoystick(int UInputFIle, I2CJoystickStatus *newStatus, I2CJoystickStatus *status) {
-
 }
 
 void startLog() {
@@ -205,11 +177,7 @@ void writeLog() {
 	fp = fopen ("log.csv","a");
 	line++;
 	fprintf (fp, "%d,%d,%d,%d,%d\n",line,rollingVoltage,amperageDifference,calculatedVoltage,indicationVoltage);
-	printf ("%d,%d,%d,%d,%d\n",line,rollingVoltage,amperageDifference,calculatedVoltage,indicationVoltage);
 	fclose(fp);
-}
-
-void readController() {
 }
 
 void calculateBattery() {
@@ -239,10 +207,9 @@ void calculateBattery() {
 	if (indicationVoltage > 3927) {chargeStatus = 8;}
 	if (indicationVoltage > 4027) {chargeStatus = 9;}
 	if (indicationVoltage > 4175) {chargeStatus = 99;}
-	if ((previousChargeStatus != chargeStatus) || (previousIsCharging != isCharging) || (previousIsMute != isMute)) {
-		// Changing Battery Status
+	if ((previousChargeStatus != chargeStatus) || (previousIsCharging != isCharging) || (previousIsMute != isMute)) { // Change Battery Status
 		char temp[512];
-		system ("sudo killall pngview");
+		system ("sudo killall pngview 2>/dev/null");
 		sprintf(temp, "/home/pi/PSPi/Driver/./pngview -n -b 0 -l 100000 -x %d -y 2 /home/pi/PSPi/Driver/PNG/battery%d%d%d.png &",resolution - 46,isMute,isCharging,chargeStatus);
 		system((char *)temp);
 	}
@@ -262,17 +229,16 @@ int main(int argc, char *argv[]) {
 		sleep(1);
     }
 	sleep(1);
-	// stores horizontal resolution to variable so this works with both LCD types
-	// would be better to grab it directly, but I'll have to work out the method
-	FILE *f = fopen("pspi.cfg","r");
-	int i;
-	char buf[4];
-	for (i = 0 ; i != 0 ; i++) {
-    		fgets(buf, 4, f);
-	}
-	fscanf(f, "%d", &resolution);
+	FILE *f = fopen("pspi.cfg","r");		// stores horizontal resolution to variable 
+	int i;									// so this works with both LCD types
+	char buf[4];							// would be better to grab it directly,
+	for (i = 0 ; i != 0 ; i++) {			// but I'll have to work out the method
+    		fgets(buf, 4, f);				//
+	}										//
+	fscanf(f, "%d", &resolution);			//
 //	startLog();
 //	int count = 0;
+	printf("Frequency is %d\n", UPDATE_FREQ);
 	while(1) {
 	    I2CJoystickStatus newStatus; // read new status from I2C
 		if(readI2CJoystick(I2CFile, &newStatus) != 0) {
@@ -295,7 +261,6 @@ int main(int argc, char *argv[]) {
 //			}
 		usleep(UPDATE_FREQ);
 	}
-
 	close(I2CFile); // close file
 	ioctl(UInputFIle, UI_DEV_DESTROY);
 }
