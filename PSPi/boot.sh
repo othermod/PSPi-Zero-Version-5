d87098861d7d004d381540a68088142ed988d323
othermod <22604540+othermod@users.noreply.github.com>
2021-12-16 11:29:32 -0500

    disable built-in LED on Pi Zero 2 W

--- PSPi/boot.sh
+++ PSPi/boot.sh
@@ -1,6 +1,11 @@
 # disable HDMI to save power
 sudo tvservice -o
 
+# disable the built-in LED on the Pi Zero 2 W
+echo 29 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio29/direction
+echo 1   > /sys/class/gpio/gpio29/value
+
 cd /home/pi/PSPi/Driver
 # check resolution and create config file
 cat /sys/class/graphics/fb0/virtual_size | cut -d, -f1 > pspi.cfg
