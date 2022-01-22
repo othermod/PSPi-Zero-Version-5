d96995036d28c099ab372bc166edcc4899a66c8e
othermod <22604540+othermod@users.noreply.github.com>
2022-01-22 14:55:54 -0500

    complete rewrite of driver

--- PSPi/boot.sh
+++ PSPi/boot.sh
@@ -7,8 +7,6 @@ echo out > /sys/class/gpio/gpio29/direction
 echo 1   > /sys/class/gpio/gpio29/value
 
 cd /home/pi/PSPi/Driver
-# check resolution and create config file
-cat /sys/class/graphics/fb0/virtual_size | cut -d, -f1 > pspi.cfg
 
 # start driver
-sudo ./pspi-controller &
+sudo ./osd -gamepad &
